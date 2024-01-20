fe548466ef2218a6b1e77e3e0d41ea3a9ce53ca6
KenzyEssam <120500567+KenzyEssam@users.noreply.github.com>
2024-01-20 17:56:33 +0200

    Upload

---
+++ Admin_pages/signin_script.js
@@ -0,0 +1,30 @@
+
+function getSelectedRole() {
+    var radioButtons = document.getElementsByName('pilih');
+
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            return radioButtons[i].value;
+        }
+    }
+    return null;
+}
+document.querySelector('form[name="sign_in_form"]').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    var username1='ahmed'
+    var password1='ahmed'
+    var radioButtons = document.getElementsByName('pilih');
+    if (username === username1 && password === password1) {
+        if (getSelectedRole()==='admin'){
+            window.location.href = './Admin_pages/home.html';
+        }
+        else{
+            window.location.href = './home.html';
+        }
+    } else {
+        alert('Incorrect username or password. Please try again.');
+    }
+});
+
